4cc6c59e91f16fddc037101d3e831b648e090d08
Vidur Mehta <vidurmehta7@gmail.com>
2023-02-03 19:52:36 +0530

    Final Commit

--- IMDBScrapper.py
+++ IMDBScrapper.py
@@ -1,5 +1,14 @@
 from bs4 import BeautifulSoup
-import requests
+import requests, openpyxl
+
+
+excel = openpyxl.Workbook()
+print(excel.sheetnames)
+sheet = excel.active
+sheet.title = 'Top Rated Movies'
+print(excel.sheetnames)
+sheet.append(['Rank', 'Movie Title', 'Year of Release', 'IMDB Rating'])
+
 
 # Connect to Website
 try:
@@ -12,7 +21,16 @@ try:
     
     for movie in movies:
         name = movie.find('td', class_="titleColumn").a.text
-        print(name)
-        break
+        
+        rank = movie.find('td', class_="titleColumn").get_text(strip= True).split('.')[0]
+        
+        year = movie.find('td', class_="titleColumn").span.text.strip('()')
+        
+        rating = movie.find('td', class_="ratingColumn imdbRating").strong.text
+        print(rank,name,year,rating)
+        sheet.append([rank,name,year,rating])
+
 except Exception as e:
     print(e)
+
+excel.save('IMDB Movie Ratings.xlsx')
